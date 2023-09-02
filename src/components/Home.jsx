52101d1b072ff774fccde8edebebb190f1d79865
Jyoti Gupta <jyo209gup201@gmail.com>
2023-09-03 01:59:15 +0530

    file added

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Codes from './Codes'
+import Result from './Result';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Codes />
+            <Result />
+        </>
+    )
+}
+
+export default Home;
